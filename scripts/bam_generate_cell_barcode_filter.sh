55049cf6481499344558e4271c9df68c7edcec82
Daniel Ringwalt <dringwalt@jhu.edu>
2024-01-17 16:25:44 -0500

    Comments for pseudobulk bam download scripts

--- scripts/bam_generate_cell_barcode_filter.sh
+++ scripts/bam_generate_cell_barcode_filter.sh
@@ -6,5 +6,4 @@ grep_string=$3
 
 
 grep '"'$sample_name'_[ATGC]*-1",.*"'$grep_string'"' $metadata | \
-  # awk -F '["_]' '{print "xf:i:25.*CB:Z:" $3; print "CB:Z:" $3 ".*xf:i:25"}'
   awk -F '["_]' '{print "CB:Z:" $3}'
--- scripts/download_bam_tx.sh
+++ scripts/download_bam_tx.sh
@@ -2,10 +2,16 @@
 
 REMOTE_FOLDER="sharepoint:Chen Lab Backups/Upd_sc"
 
+# Path to scRNA-seq-Metadata.csv
 metadata=$1
+# condition/driver: Nos or Tj. However, the argument is expected as lowercase.
 condition_lower="${2%.*}"
+# Batch: Replicate of the Nos-driven or Tj-driven experiment (numbered 1 or 2).
 batch_number="${2#*.}"
+# Grep the csv file for this string, then get the record containing the cell
+# barcode and print that to a file for searching SAM output.
 grep_string=$3
+# Output TSV: transcript id and pseudobulk count.
 output_path=$4
 
 cb_file=`mktemp`
@@ -19,5 +25,6 @@ fi
 
 rclone cat "$REMOTE_FOLDER/$dir_name/outs/possorted_genome_bam.bam" | \
     samtools view | grep -F xf:i:25 | grep -F -f $cb_file | \
+    # The match follows a TX:Z: tag name, or a previous match \G followed by ;.
     perl -ne 'for my $m (/(?:TX:Z:|\G;)(FBtr\d+)[^;]+/g) { print "$m\n" }' | \
     sort | uniq -c > $output_path
