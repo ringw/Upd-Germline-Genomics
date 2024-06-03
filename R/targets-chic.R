3580079080f31b583f2c7b0862fd62f3f39737ce
Daniel Ringwalt <dringw@gmail.com>
2024-06-03 17:46:22 -0400

    Adding chic tile wide - use correct tar_map

--- R/targets-chic.R
+++ R/targets-chic.R
@@ -280,9 +280,7 @@ targets.chic <- list(
           str_glue("chic.bam_{sample}_{name}"),
           SIMPLIFY=F
         ),
-        chic.tile.diameter_40 = rlang::syms(str_glue("chic.tile.diameter_40_{name}")),
-        chic.tile.diameter_1000 = rlang::syms(str_glue("chic.tile.diameter_1000_{name}")),
-        chic.tile.diameter_1000_lookup = rlang::syms(str_glue("chic.tile.diameter_1000_lookup_{name}"))
+        chic.tile.diameter_40 = rlang::syms(str_glue("chic.tile.diameter_40_{name}"))
       ),
     names = suffix,
 
@@ -385,6 +383,8 @@ targets.chic <- list(
           list,
         chic.granges.diameter_40 = rlang::syms(str_glue("chic.granges.diameter_40_{chic_sample_names}_{bp_suffix}_{reference}")) %>%
           list,
+        chic.tile.diameter_1000 = rlang::syms(str_glue("chic.tile.diameter_1000_{name}")),
+        chic.tile.diameter_1000_lookup = rlang::syms(str_glue("chic.tile.diameter_1000_lookup_{name}")),
         experimental_group = (
           chic.samples$group[match(chic_sample_names, chic.samples$sample)]
         ) %>%
