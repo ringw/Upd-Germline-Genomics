f9c2a3de01d7e23691f07cecf9203999648569e8
Daniel Ringwalt <dringwalt@jhu.edu>
2024-09-03 13:07:36 -0400

    Update devianceResiduals to handle n == 0

--- R/deviance-residuals.R
+++ R/deviance-residuals.R
@@ -2,13 +2,13 @@ devianceResiduals <- function(Y) {
   n <- colSums(Y)
   N <- matrix(n, nrow = nrow(Y), ncol = ncol(Y), byrow = TRUE)
   fit <- glm_gp(
-    Y,
-    size_factors = n,
+    Y[, n != 0],
+    size_factors = n[n != 0],
     overdispersion = FALSE,
     overdispersion_shrinkage = FALSE,
     verbose = TRUE
   )
-  Mu <- fit$Mu
+  Mu <- fit$Mu[, match(seq(ncol(Y)), which(n != 0))]
   sign(Y - Mu) *
     sqrt(
       2 * Y * log(Y / Mu)
