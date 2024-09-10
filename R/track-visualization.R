2d0c772056ca13f742b5caaf657fcd3fc6a4884a
Daniel Ringwalt <dringwalt@jhu.edu>
2024-09-10 16:58:29 -0400

    Update track visualization aesthetics.

--- R/track-visualization.R
+++ R/track-visualization.R
@@ -6,31 +6,25 @@ plot_track <- function(
     limits = c(-1, 1),
     breaks = c(-1, 0, 1),
     subsample_draw = TRUE) {
-  seq_offsets <- seqnames(track) %>%
-    as.factor() %>%
-    fct_relabel(
-      \(names) names %>%
-        sapply(\(n) if (n == "2R") chr.lengths["2L"] else if (n == "3R") chr.lengths["3L"] else 0) %>%
-        as.character()
-    )
   df <- tibble(
-    chr = fct_recode(as.factor(seqnames(track)), `2` = "2L", `2` = "2R", `3` = "3L", `3` = "3R"),
-    pos = mid(track) + as.numeric(as.character(seq_offsets)),
+    chr = as.factor(seqnames(track)),
+    pos = mid(track),
     value = track$score
   ) %>%
-    subset(chr %in% c("2", "3", "4", "X", "Y"))
+    subset(chr %in% names(chr.lengths))
   chr_lookup <- as.numeric(df$chr)
   chr_levels <- list(`2` = 1, `3` = 2, `4` = 3, `X` = 4, `Y` = 5)
   if (subsample_draw)
     df <- df %>%
       dplyr::slice(
-        c(
-          round(seq(1, -1 + match(chr_levels$`3`, chr_lookup), length.out = 1000)),
-          round(seq(match(chr_levels$`3`, chr_lookup), -1 + match(chr_levels$`4`, chr_lookup), length.out = 1000)),
-          round(seq(match(chr_levels$`4`, chr_lookup), -1 + match(chr_levels$X, chr_lookup))),
-          round(seq(match(chr_levels$X, chr_lookup), -1 + match(chr_levels$Y, chr_lookup), length.out = 500)),
-          round(seq(match(chr_levels$Y, chr_lookup), nrow(df), length.out = 1000))
-        )
+        df %>%
+          group_by(chr) %>%
+          reframe(
+            keep = seq(length(pos)) %in% round(seq(1, length(pos), length.out=1000)) |
+              chr == "4"
+          ) %>%
+          pull(keep) %>%
+          which()
       )
   plot_chr <- function(chr.) ggplot(subset(df, chr == chr.), aes(pos, value)) +
     annotate(
@@ -74,38 +68,57 @@ plot_track <- function(
     gr$widths[9] <- margin_right
     gr <- gr
   }
+  # Contents width will be 5.25 in. Two-column (chromosome arms) layout will be
+  # handled by the two arms having precisely 2 * default margins (5.5 pt) in
+  # between the columns, and no other middle content (axis title, axis text).
   chrX <- (
     plot_chr("X") +
-      labs(title = "X")
+      labs(title = "Chromosome X")
   ) %>%
     ggplot_build_panel_absolute(
-      unit(0.35, "in"), unit(4, "in"), unit(5.5, "points") + unit(1, "in")
+      unit(0.35, "in"), unit(4.5, "in"), unit(0.55, "in") + unit(11, "points")
     )
-  chr2 <- (
-    plot_chr("2") +
-      annotate("line", rep(chr.lengths["2L"], 2), c(-Inf, Inf)) +
-      labs(title = "2")
-  ) %>%
-    ggplot_build_panel_absolute(unit(0.35, 'in'), unit(5, 'in'))
-  chr3 <- (
-    plot_chr("3") +
-      annotate("line", rep(chr.lengths["3L"], 2), c(-Inf, Inf)) +
-      labs(title = "3")
-  ) %>%
-    ggplot_build_panel_absolute(unit(0.35, 'in'), unit(5, 'in'))
+  chr2 <- cbind(
+    (plot_chr("2L") + labs(title = "Chromosome 2")) %>%
+      ggplot_build_panel_absolute(unit(0.35, "in"), unit(2.5, "in")),
+    (
+      plot_chr("2R") +
+        theme(
+          axis.title = element_blank(),
+          axis.text = element_blank()
+        )
+    ) %>%
+      ggplot_build_panel_absolute(
+        unit(0.35, "in"), unit(2.5, "in"), unit(0.05, "in")
+      )
+  )
+  chr3 <- cbind(
+    (plot_chr("3L") + labs(title = "Chromosome 3")) %>%
+      ggplot_build_panel_absolute(unit(0.35, "in"), unit(2.5, "in")),
+    (
+      plot_chr("3R") +
+        theme(
+          axis.title = element_blank(),
+          axis.text = element_blank()
+        )
+    ) %>%
+      ggplot_build_panel_absolute(
+        unit(0.35, "in"), unit(2.5, "in"), unit(0.05, "in")
+      )
+  )
   chr4 <- (
     plot_chr("4") +
-      labs(title = "4")
+      labs(title = "Chromosome 4")
   ) %>%
     ggplot_build_panel_absolute(
-      unit(0.35, 'in'), unit(2, 'in'), unit(5.5, 'points') + unit(3, 'in')
+      unit(0.35, "in"), unit(2, "in"), unit(3.05, "in") + unit(11, "points")
     )
   chrY <- (
     plot_chr("Y") +
-      labs(title = "Y")
+      labs(title = "Chromosome Y")
   ) %>%
     ggplot_build_panel_absolute(
-      unit(0.35, "in"), unit(2, "in"), unit(5.5, "points") + unit(3, "in")
+      unit(0.35, "in"), unit(2, "in"), unit(3.05, "in") + unit(11, "points")
     )
   mylayout <- gtable(
     widths = unit(1, 'null'), heights = unit(rep(0.75, 5), 'in')
