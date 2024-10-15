2e26efbeb6cc77cb2025b59801987f6c6e558ab0
Daniel Ringwalt <dringwalt@jhu.edu>
2024-10-15 13:45:07 -0400

    Revert "Delete unneeded masked reference"
    This reverts commit 0e1e8ab3ae49032e4f1a30abe420345efe85f993.

--- R/patterns.R
+++ R/patterns.R
@@ -3,9 +3,7 @@ bowtie.refs <- tribble(
   "chr",
   rlang::sym("bowtie_chr"),
   rlang::syms("fasta_flybase"),
-  # Currently no use for the RepeatMasker & histone repeat unit & transposon
-  # sequence set reference!
-  # "masked",
-  # rlang::sym("bowtie_masked"),
-  # rlang::syms(c("fasta_masked", "fasta_histone_unit", "fasta_transposon_sequence_set"))
+  "masked",
+  rlang::sym("bowtie_masked"),
+  rlang::syms(c("fasta_masked", "fasta_histone_unit", "fasta_transposon_sequence_set"))
 )
